31c2123d42d5f87eb99697de175830f979cc7e65
Juan <saraviamartin16@hotmail.com>
2023-09-06 12:23:39 -0300

    usando reactFC y type vs interface probando

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react"
 
+const mockTodos = [
+  {
+    id: '1',
+    title: 'todo 1',
+    completed: false,
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    completed: false,
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    completed: false,
+  },
+]
 
 const  App = (): JSX.Element  => {
+  const [todos] = useState(mockTodos)
   return (
-    <>
-      <h1>todo mvc</h1>
-    </>
+    <Todos todos={todos}/>
   )
 }
 
